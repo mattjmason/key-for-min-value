36791c2a2dc44de9e3a2f858f0a825a1221b671a
Matt Mason <masonmj10@gmail.com>
2020-11-20 00:27:54 -0500

    commit tree 845b65898566173e4e26df11df3d2063128e77e7

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,13 @@
 
 def key_for_min_value(name_hash)
 
+    min_key = nil
+    min_value = nil
+    name_hash.each do |key, value|
+        if min_key == nil || value < min_value
+            min_value = value
+            min_key = key
+        end 
+    end 
+    min_key
 end
